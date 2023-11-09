147eead7d78be3f873acdf02bbc83032e4a8bac6
Mohsin <oxgmohsin@gmail.com>
2023-11-09 20:19:52 +0600

    data.js added into main branch

---
+++ src/data.js
@@ -0,0 +1,63 @@
+const arrayOfQuotes = [
+  {
+    qoute:
+      "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+    author: "Nelson Mandela",
+  },
+  {
+    qoute: "The way to get started is to quit talking and begin doing. ",
+    author: "Walt Disney",
+  },
+  {
+    qoute:
+      "The future belongs to those who believe in the beauty of their dreams. ",
+    author: "Eleanor Roosevelt",
+  },
+  {
+    qoute:
+      "If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough. ",
+    author: "Oprah Winfrey",
+  },
+  {
+    qoute: "You must be the change you wish to see in the world. ",
+    author: "Mahatma Gandhi",
+  },
+  {
+    qoute:
+      "Spread love everywhere you go. Let no one ever come to you without leaving happier. ",
+    author: "Mother Teresa",
+  },
+  {
+    qoute: "The only thing we have to fear is fear itself. ",
+    author: "Franklin D. Roosevelt",
+  },
+  {
+    qoute: "Do one thing every day that scares you. ",
+    author: "Eleanor Roosevelt",
+  },
+  {
+    qoute: "Well done is better than well said. ",
+    author: "Benjamin Franklin",
+  },
+  {
+    qoute:
+      "It is during our darkest moments that we must focus to see the light.",
+    author: " Aristotle",
+  },
+  {
+    qoute:
+      "Do not go where the path may lead, go instead where there is no path and leave a trail. ",
+    author: "Ralph Waldo Emerson",
+  },
+  {
+    qoute: "Be yourself; everyone else is already taken. ",
+    author: "Oscar Wilde",
+  },
+  {
+    qoute:
+      "You will face many defeats in life, but never let yourself be defeated",
+    author: "Maya Angelou",
+  },
+];
+
+export { arrayOfQuotes };
