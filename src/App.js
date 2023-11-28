7b6b641e138c641a9c392f2b06d87921be4e62ae
Mohsin <oxgmohsin@gmail.com>
2023-11-28 23:08:43 +0600

    sending parent's use state values to child components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
+import { useState } from "react";
 import "./App.css";
 import Card from "./components/Card";
 
 function App() {
+  const [parentTheme, setParentTheme] = useState("blue");
+
+  const handleParentTheme = () => {
+    if (parentTheme === "blue") {
+      setParentTheme("yellow");
+    } else {
+      setParentTheme("blue");
+    }
+  };
   return (
-    <div id="container">
-      <Card name="Mohsin" designation="Software Developer" />
-      <Card name="Mitu" designation="Software Engineer" />
-      <Card name="Imran Hossain" designation="Business Magnet" />
+    <div id={parentTheme === "blue" ? "container-blue" : "container-yellow"}>
+      <Card
+        name="Mohsin"
+        designation="Software Developer"
+        setParentTheme={setParentTheme}
+        parentTheme={parentTheme}
+      />
+      {/* <button id="btn" onClick={handleParentTheme}>
+        Change Parent div
+      </button> */}
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,42 @@
+import { useState, useEffect } from "react";
 import "./Card.css";
 
 function Card(props) {
-  //  props = {name:"Mohsin", designation:"Software Developer" }
+  //  props = {name:"Mohsin", designation:"Software Developer", parentTheme:parentTheme setParentTheme: setParentTheme}
+
+  const [childTheme, setChildTheme] = useState("dark");
+  console.log(props);
+
+  const handleChildTheme = () => {
+    if (childTheme === "dark") {
+      setChildTheme("light");
+    } else {
+      setChildTheme("dark");
+    }
+
+    if (props.parentTheme === "blue") {
+      props.setParentTheme("yellow");
+    } else {
+      props.setParentTheme("blue");
+    }
+  };
+
+  // useEffect(() => {
+  //   fetch("https://jsonplaceholder.typicode.com/post")
+  //     .then((response) => response.json())
+  //     .then((json) => console.log(json));
+  // });
+
   return (
-    <div id="center">
+    <div id={childTheme === "dark" ? "center-dark" : "center-light"}>
       <div> {props.name} </div>
       <div>{props.designation}</div>
+      <button
+        id={childTheme === "dark" ? "btn-light" : "btn-dark"}
+        onClick={handleChildTheme}
+      >
+        Change theme
+      </button>
     </div>
   );
 }
