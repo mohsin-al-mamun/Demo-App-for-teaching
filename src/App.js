24cae9d3de5fd3b7b8757e96bdc830f672e6f0eb
Mohsin <oxgmohsin@gmail.com>
2023-12-10 23:24:14 +0600

    useeffect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
+import { useState } from "react";
 import "./App.css";
 import Card from "./components/Card";
+import { useEffect } from "react";
 
 function App() {
+  const [state, setState] = useState("posts");
+  const [data, setData] = useState([]);
+
+  const handleState = (e) => {
+    setState(e.target.value);
+  };
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${state}`)
+      .then((response) => response.json())
+      .then((json) => setData(json));
+  }, [state]);
+
+  console.log("The current state is", state);
+  console.log("This is a random line");
+  console.log("MMMMMMMMMMMMMMM", data);
   return (
     <div id="container">
-      <Card name="Mohsin" designation="Software Developer" />
-      <Card name="Mitu" designation="Software Engineer" />
-      <Card name="Imran Hossain" designation="Business Magnet" />
+      <button className="btn" value="posts" onClick={handleState}>
+        Post
+      </button>
+      <button className="btn" value="comments" onClick={handleState}>
+        Comments
+      </button>
+      <button className="btn" value="users" onClick={handleState}>
+        Users
+      </button>
+      <div> {state} is Clicked!! </div>
     </div>
   );
 }
